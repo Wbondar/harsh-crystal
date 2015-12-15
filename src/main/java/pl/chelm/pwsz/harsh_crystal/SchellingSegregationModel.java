06a17eb4ab0b11aa1fae0eb8957d8939477e64e3
Vladyslav Bondarenko <vladyslavbond@protonmail.ch>
2015-12-15 14:24:40 +0100

    This resolves Wbondar/harsh-crystal#16.

--- src/main/java/pl/chelm/pwsz/harsh_crystal/SchellingSegregationModel.java
+++ src/main/java/pl/chelm/pwsz/harsh_crystal/SchellingSegregationModel.java
@@ -1,6 +1,7 @@
 package pl.chelm.pwsz.harsh_crystal;
 
 import java.util.Optional;
+import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -32,31 +33,41 @@ public final class SchellingSegregationModel extends Simulation {
 		System.out.println(String.format("Check (%d, %d).", x, y));
 		if (isUnsatisfied(x, y)) {
 			System.out.println(String.format("Dweller of (%d, %d) is unsatisfied.", x, y));
-			relocateToNearestEmptyCellOrDie(x, y);
+			relocateToRandomEmptyCellOrDie(x, y);
 			return true;
 		}
 		return false;
 	}
-	
-	private final void relocateToNearestEmptyCellOrDie(final int x0, final int y0) {
+	/**
+	 * The method assumes, that at least one empty cell exists on the board.
+	 * @param x0 position x of actor to relocate
+	 * @param y0 position y of actor to relocate
+	 */
+	private final void relocateToRandomEmptyCellOrDie(final int x0, final int y0) {
 		final Board board = getBoard();
-		for (int x = x0 - 1; x >= 0 && x < board.getWidth(); x++) {
-			for (int y = y0 - 1; y >= 0 && y < board.getHeight(); y++) {
-				if (board.isEmpty(x, y)) {
-					System.out.println(String.format("Move %d from (%d, %d) to (%d, %d).", board.getCellTypeId(x0, y0), x0, y0, x, y));
-					final int cellTypeId = board.getCellTypeId(x0, y0);
-					board.swap(x0, y0, x, y);
-					assert board.isEmpty(x0, y0) == true;
-					assert board.getCellTypeId(x, y) == cellTypeId;
-					return;
-				}
-			}
+		Random random = new Random();
+		int x = random.nextInt(board.getWidth() - 1);
+		int y = random.nextInt(board.getHeight() - 1);
+		if (board.isEmpty(x, y)) {
+			System.out.println(String.format("Move %d from (%d, %d) to (%d, %d).", board.getCellTypeId(x0, y0), x0, y0, x, y));
+			final int cellTypeId = board.getCellTypeId(x0, y0);
+			board.swap(x0, y0, x, y);
+			assert board.isEmpty(x0, y0) == true;
+			assert board.getCellTypeId(x, y) == cellTypeId;
+			return;
+		} else {
+			relocateToRandomEmptyCellOrDie(x0, y0);
 		}
 		System.out.println(String.format("Kill %d at (%d, %d).", board.getCellTypeId(x0, y0), x0, y0));
 		board.emptyCell(x0, y0);
 		assert board.isEmpty(x0, y0) == true;
 	}
-
+	/**
+	 * 
+	 * @param x0 position x of actor to check satisfaction
+	 * @param y0 position y of actor to check satisfaction
+	 * @return `true` if percent of non-kin of occupant is greater than occupant can tolerate and `false` otherwise; `false` if cell of coordinates (`x0`, `y0`) is inoccupied.
+	 */
 	private boolean isUnsatisfied(int x0, int y0) {
 		final Board board = getBoard();
 		if (board.isEmpty(x0, y0) || board.isUnreachable(x0, y0)) {
